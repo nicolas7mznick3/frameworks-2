d1380ec6c26686c7f3b468221d65c351fe1b79ab
Daniela Bazan Pavão <dpavao145@gmail.com>
2024-09-04 09:29:43 -0300

    Alterado nome do arquivo configgit para configgit.bat

--- configgit.bat
+++ configgit.bat
@@ -1,3 +1,3 @@
 cmdkey /delete git://github.com
-git config --global user.nmae "Nicolas Bonacina"
+git config --global user.name "Nicolas Bonacina"
 git config --global user.email sansbbonacina@gmail.com
---
+++ index.js
@@ -0,0 +1,3 @@
+function teste() {
+    console.log('Bem vindo ao JS')
+}
